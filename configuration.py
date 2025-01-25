8c887059ebe1771234de7ac65e96c477c547fe1d
Crosty02 <crystal.puentes31@gmail.com>
2025-01-24 22:25:06 -0500

    my first project phyton

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-bb196f8b-9b58-44e1-9560-255c21e6d3aa.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
